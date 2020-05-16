86167ae7c50c175e9b0f311064de42d0a0c74aca
pluiel <pluiel@outlook.com>
2020-05-15 23:10:37 -0700

    Initial Commit

---
+++ MentorBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MentorBook
+//
+//  Created by Yuka Uematsu on 2020/05/15.
+//  Copyright © 2020 Uematsu Well. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
